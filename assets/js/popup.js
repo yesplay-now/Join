64f8017b4f7ceb93bd9900f4926ad3d0643971ee
yesplay-now <partnersbuffallo@gmail.com>
2026-04-15 12:02:49 +0530

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -145,7 +145,7 @@
 //         helper.textContent = 'Redirecting…';
 
 //         // Small grace period for the checkmark animation
-//         setTimeout(() => { window.location.href = 'http://h2n6.com/?utm_campaign=sOGcfWKDHx&v1=[v1]&v2=[v2]&v3=[v3]'; }, 600);
+//         setTimeout(() => { window.location.href = 'http://t1l9.com/?utm_campaign=clJqcMWRQZ&v1=[v1]&v2=[v2]&v3=[v3]'; }, 600);
 //       }, 5000);
 //     }, 300); // "few milliseconds" before starting the spinner
 //   }
@@ -299,7 +299,7 @@
 //         helper.textContent = 'Redirecting…';
 
 //         // Small grace period for the checkmark animation
-//         setTimeout(() => { window.location.href = 'http://h2n6.com/?utm_campaign=sOGcfWKDHx&v1=[v1]&v2=[v2]&v3=[v3]'; }, 600);
+//         setTimeout(() => { window.location.href = 'http://t1l9.com/?utm_campaign=clJqcMWRQZ&v1=[v1]&v2=[v2]&v3=[v3]'; }, 600);
 //       }, 5000);
 //     }, 300); // "few milliseconds" before starting the spinner
 //   }
